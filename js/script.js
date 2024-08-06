0647f177dba58129f821838d83eb32c9a7977fe6
Ruslan <ruslas@yandex.ru>
2024-08-07 00:41:48 +0300

    адаптивность при 320, add js burger-menu & slider indicator, добавление префиксов для кроссбраузерности

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const btnBurger = document.querySelector('.header__burger-btn img'),
+document.addEventListener('DOMContentLoaded', () => {
+  const btnBurger = document.querySelector('.header__burger-btn img'),
     btnClose = document.querySelector('.burger-menu__btn-close'),
     burgerMenu = document.querySelector('.burger-menu');
 
-    btnBurger.addEventListener('click', (e)=>{
-        e.preventDefault();
-        if(burgerMenu.classList.contains('hidden')){
-            burgerMenu.classList.add('open');
-            burgerMenu.classList.remove('hidden')
+  // Burger Menu
+  btnBurger.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (burgerMenu.classList.contains('hidden')) {
+      burgerMenu.classList.add('open');
+      burgerMenu.classList.remove('hidden');
+    }
+    console.log(e.target);
+  });
+  btnClose.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (burgerMenu.classList.contains('open')) {
+      burgerMenu.classList.add('hidden');
+      burgerMenu.classList.remove('open');
+    }
+  });
 
-        }
-        console.log(e.target)
+  // Slider Indicator
+  const linksSlider = document.querySelectorAll('[data-name]'),
+    indicatorSlider = document.querySelector('.header__indicator');
+  linksSlider.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      let target = e.target,
+        classes = Array.from(indicatorSlider.classList);
+      indicatorSlider.className = '';
+      if (target.dataset.name === 'start')
+        classes[1] = 'header__indicator_start';
+      else if (target.dataset.name === '01')
+        classes[1] = 'header__indicator_01';
+      else if (target.dataset.name === '02')
+        classes[1] = 'header__indicator_02';
+      else if (target.dataset.name === '03')
+        classes[1] = 'header__indicator_03';
+      indicatorSlider.classList.add(...classes);
     });
-    btnClose.addEventListener('click',(e)=>{
-        e.preventDefault();
-        if(burgerMenu.classList.contains('open')){
-            burgerMenu.classList.add('hidden');
-            burgerMenu.classList.remove('open');
-        }
-    })
+  });
 });
